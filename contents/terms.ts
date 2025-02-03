1ab396b3cc33b0e5525f1e17f05d7cb2be7acfac
Harold <75729237+surelle-ha@users.noreply.github.com>
2025-02-03 19:54:15 +0800

    chore: update content

--- contents/terms.ts
+++ contents/terms.ts
@@ -4,30 +4,24 @@ export default [
 		icon: "i-heroicons-information-circle",
 		defaultOpen: true,
 		content:
-			"lol Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.",
+			"Oxide OpenSource Philippines is an open community for developers of all backgrounds and tech stacks. Whether you're a beginner, a seasoned engineer, or simply passionate about open-source, you're welcome here!",
 	},
 	{
-		label: "Installation",
+		label: "App Installation",
 		icon: "i-heroicons-arrow-down-tray",
 		content:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.",
+			"Our app is currently in development and will be available soon on Android and iOS! 📱✨ We're working hard to bring you an amazing experience, and we can't wait to share it with you. Stay tuned for updates, and follow us for the latest news!",
 	},
 	{
-		label: "Layouts",
-		icon: "i-heroicons-rectangle-group",
-		content:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.",
-	},
-	{
-		label: "Components",
+		label: "Perks & Benefits",
 		icon: "i-heroicons-square-3-stack-3d",
 		content:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.",
+			"By becoming part of Oxide OpenSource Philippines, you’ll gain access to a vibrant and inclusive developer community. ",
 	},
 	{
-		label: "Utilities",
+		label: "Free Tools",
 		icon: "i-heroicons-wrench-screwdriver",
 		content:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.",
+			"We're developing an open-source toolkit—an NPM package designed to help developers build powerful and efficient backend servers with ease. Our goal is to simplify development, boost productivity, and provide a solid foundation for building scalable applications. But we’re not stopping there! In the future, this toolkit will expand beyond backend development, offering utilities and tools for various aspects of software development.",
 	},
 ];
