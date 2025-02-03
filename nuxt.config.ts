90bf30260fd8004760128f7f3017faa59e22f48f
Harold <75729237+surelle-ha@users.noreply.github.com>
2025-02-03 17:38:28 +0800

    dev: enable routing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,7 @@ export default defineNuxtConfig({
 	compatibilityDate: "2024-11-01",
 	devtools: { enabled: true },
 	modules: ["@nuxt/ui", "@pinia/nuxt", "@vueuse/nuxt"],
+	runtimeConfig: {
+		public: { apiBaseUrl: process.env.API_BASE_URL || "http://localhost:3000" },
+	},
 });
